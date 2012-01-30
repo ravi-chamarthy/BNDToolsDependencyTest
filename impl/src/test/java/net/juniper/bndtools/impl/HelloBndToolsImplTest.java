85b8820887099aaa912364d9c5fb56dcb88393da
Ravi Chamarthy <rchamarthy@juniper.net>
2012-01-30 11:40:57 -0800

    Add Unit test case.

--- impl/src/test/java/net/juniper/bndtools/impl/HelloBndToolsImplTest.java
+++ impl/src/test/java/net/juniper/bndtools/impl/HelloBndToolsImplTest.java
@@ -4,4 +4,9 @@ import junit.framework.TestCase;
 
 public class HelloBndToolsImplTest extends TestCase {
 
+	public void testSayHello() {
+		HelloBndToolsImpl hello = new HelloBndToolsImpl();
+		hello.sayHello();
+	}
+
 }
